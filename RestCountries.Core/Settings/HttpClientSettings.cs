180c81e89c4b8a9060aa15179cfb5c20e11b545a
Johnn <Johnn@LAPTOP-B5NN8U8V>
2025-06-12 20:01:02 +0300

    HttpClientSettings for Retry policy
    HttpClientSettings for Retry policy

---
+++ RestCountries.Core/Settings/HttpClientSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace RestCountries.Core.Settings
+{
+    public sealed class HttpClientSettings
+    {
+        public int RetryCount { get; set; }
+        public int SleepDuration { get; set; }
+    }
+}
